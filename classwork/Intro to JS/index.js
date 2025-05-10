bd288f74e0fa87bd1d2904619e3636ea2aaa796c
Ravan Abdinli <rabdinli17966@ada.edu.az>
2025-05-10 13:09:32 +0300

    adding classwork folder

---
+++ classwork/Intro to JS/index.js
@@ -0,0 +1,48 @@
+// var usname = prompt("What is your name?");
+// var num = prompt("How old are you? ");
+
+// alert("Salam," + usname + ", Sizin yaşınız:"+ num + "dur.");
+
+
+
+
+// const  r1 = parseInt(prompt("birinci reqemi daxil et:"))
+// const r2 = parseInt(prompt("ikinici reqemi daxil et:"))
+// const sum = r1 + r2
+
+// alert("Iki ededin cemi:" + sum);
+
+
+
+// var x = parseInt(prompt("Eded daxil et:"))
+
+// if (x % 2 == 0 ) {
+//     alert("Bu eded cutdur")
+// }else{
+//     alert("Bu eded tekdir")
+// }
+
+
+// var eded = parseInt(prompt("Qiymetini daxil et:"));
+
+// if (eded <= 49) {
+//     alert("Zeif");
+// } else if (eded >= 50 && eded <= 69) {
+//     alert("Kafi");
+// } else if (eded >= 70 && eded <= 89) {
+//     alert("Yaxsi");
+// } else {
+//     alert("Ela");
+// }
+
+
+// var eded1 = parseInt(prompt("Eded1 daxil et:"));
+// var eded2 = parseInt(prompt("Eded2 daxil et:"));
+// var eded3 = parseInt(prompt("Eded3 daxil et:"));
+
+// if (eded1 >= eded2 && eded1 >= eded3) {
+//     alert("En boyuk eded: " + eded1);
+// } else if (eded2 >= eded1 && eded2 >= eded3) {
+//     alert("En boyuk eded: " + eded2);
+// } else {
+//     alert("En boyuk eded: " + eded3);}

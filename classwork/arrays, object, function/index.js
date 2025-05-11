7076cc72bd1a80491eff02a779d4ce72483e9fa8
Ravan Abdinli <rabdinli17966@ada.edu.az>
2025-05-11 12:34:33 +0300

    functions in class

---
+++ classwork/arrays, object, function/index.js
@@ -0,0 +1,60 @@
+// var ad = prompt("Adın:")
+// var soyad = prompt ("Soyadın:")
+
+// function username($ad, $soyad) {
+//     return("Salam, " + $ad + " " + $soyad)    
+// }
+
+// console.log(username(ad,soyad))
+
+
+
+
+// var n =  parseInt(prompt("Eded:"))
+
+// function calcFactorial(n) {
+  
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+ 
+// }
+
+// console.log(calcFactorial(n));
+
+
+// var number =  parseInt(prompt("Eded:"))
+
+// const findOddOrEven = (number) =>{
+
+// if (number % 2 == 0 ) {
+//     alert("Bu eded cutdur")
+// }else{
+//     alert("Bu eded tekdir")
+// }
+
+// }
+
+// console.log(findOddOrEven(number));
+
+
+
+
+// var sentece =  prompt("Cumle:")
+
+// function reverseStrIng(sentece){
+// return sentece.split("").reverse().join("")
+// }
+
+// console.log(reverseStrIng(sentece))
+
+
+
+// var sentence = prompt("Cumle")
+// function capitalize(sentence) {
+//     return sentence.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+// }
+
+// console.log(capitalize(sentence)); 
